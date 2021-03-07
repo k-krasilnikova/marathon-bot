33e9a24db58f651b8870ef0e2fd5e765b06b4c7a
Katsiaryna Krasilnikova <katsiaryna@MacBook-Pro-Katsiaryna.local>
2021-03-07 15:00:32 +0300

    Changed time for jobs

--- env.js
+++ env.js
@@ -1,9 +1,9 @@
 export const CONFIG = {
   TOKEN: "1522199656:AAFkeep3WPHfrZ-o2wsJHMZnFI9lSEsuZuY",
   REVIEWER: "314829300", //Kate | "558921837" Polina | "410932490"; Denis | "558921837"; Polina
-  SCHEDULE_TIME: "58 11 * * *", //"00 19 * * *", //
-  SCHEDULE_TIME_ANGRY: "30 14 * * *", //"0 08 * * *", //
-  SCHEDULE_TIME_INFO: "41 14 * * *", //"0 15 */3 * *", //
+  SCHEDULE_TIME: "0 16 * * *", //"00 19 * * *", //
+  SCHEDULE_TIME_ANGRY: "0 05 * * *", //"0 08 * * *", //
+  SCHEDULE_TIME_INFO: "0 12 */3 * *", //"0 15 */3 * *", //
   DB:
     "mongodb+srv://kattrinkr:123qweASD@sirvel-marathon-bot.zemcb.mongodb.net/sirvel-marathon-bot",
 };
