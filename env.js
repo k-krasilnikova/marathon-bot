14a8b8a2fad9d62ec1a6bef1e328b050c5a88742
Katsiaryna Krasilnikova <katsiaryna@MacBook-Pro-Katsiaryna.local>
2021-03-07 14:56:46 +0300

    Added work with real trainer

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   TOKEN: "1522199656:AAFkeep3WPHfrZ-o2wsJHMZnFI9lSEsuZuY",
   REVIEWER: "314829300", //Kate | "558921837" Polina | "410932490"; Denis | "558921837"; Polina
-  SCHEDULE_TIME: "55 14 * * *", //"00 19 * * *", //
+  SCHEDULE_TIME: "58 11 * * *", //"00 19 * * *", //
   SCHEDULE_TIME_ANGRY: "30 14 * * *", //"0 08 * * *", //
   SCHEDULE_TIME_INFO: "41 14 * * *", //"0 15 */3 * *", //
   DB:
